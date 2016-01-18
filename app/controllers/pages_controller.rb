3601ac1850cb9c3c7da83ca7527ebbacca6798e1
vinszav <vicentezavarcen@gmail.com>
2016-01-18 16:11:31 -0500

    Created dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
+  
+  before_action :authenticate_user!, only: [:dashboard]
+
   def home
   	if current_user 
   		redirect_to bags_path
   end
 end
+
+def dashboard
+	@bags = current_user.bags
+	end
 end
